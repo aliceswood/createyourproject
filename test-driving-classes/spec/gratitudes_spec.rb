3814005c61d0bdf36a52c932102ea2f6b57b22c0
Alice Wood <alicesophiewood@gmail.com>
2023-04-14 14:46:04 +0100

    gratitudes class file and rspec test file

--- test-driving-classes/spec/gratitudes_spec.rb
+++ test-driving-classes/spec/gratitudes_spec.rb
@@ -1,10 +1,10 @@
-# gratitudes = Gratitudes.new
-# gratitudes.add("your cat")
-# gratitudes.add("nice people")
-# expect(gratitudes.format).to eq "Be grateful for: your cat, nice people"
-
 require 'gratitudes'
 
-RSPec.descrive Gratitudes do
-  
+RSpec.describe Gratitudes do
+  it do
+    gratitudes = Gratitudes.new
+    gratitudes.add("your cat")
+    gratitudes.add("nice people")
+    expect(gratitudes.format).to eq "Be grateful for: your cat, nice people"
+  end
 end
