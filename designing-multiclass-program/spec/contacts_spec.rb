9deb20d834f533ac35bbab74101ca36b430eed6e
Alice Wood <alicesophiewood@gmail.com>
2023-04-21 13:33:24 +0100

    removing commented out test

--- designing-multiclass-program/spec/contacts_spec.rb
+++ designing-multiclass-program/spec/contacts_spec.rb
@@ -1,8 +1,5 @@
 require 'contacts'
 
 RSpec.describe Contact do
-  # it "extracts numbers and saves to list" do
-  #   contact = Contact.new
-  #  expect(contact.extract_number("my number is 07000000001")).to eq "07000000001"
-  # end
+
 end
